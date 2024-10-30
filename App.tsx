ff98e22bb5ec01d769b2eda1c80bf8b276471442
Henrique Fontoura <henriquefontoura.exe@gmail.com>
2024-10-30 19:53:30 -0300

    Initial commit - projeto SafeK8s

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <LoginScreen />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#15616D', // Cor de fundo da tela
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      {/* Conteúdo da tela será adicionado aqui */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#15616D', // Cor de fundo
+    alignItems: 'center', // Centraliza horizontalmente
+    justifyContent: 'center', // Centraliza verticalmente
+  },
+});
